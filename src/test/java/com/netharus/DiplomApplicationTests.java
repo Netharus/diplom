bb4c4f1dc3f69314014d823506ae247c84510f69
Netharus <nekromant12331@gmail.com>
2025-03-23 17:52:52 +0300

    build!: project init

---
+++ src/test/java/com/netharus/DiplomApplicationTests.java
@@ -0,0 +1,15 @@
+package com.netharus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class DiplomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/netharus/TestDiplomApplication.java
@@ -0,0 +1,11 @@
+package com.netharus;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDiplomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(DiplomApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
