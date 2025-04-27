21a0155df6a9a800c738a01eb20fa02c249e905b
Netharus <nekromant12331@gmail.com>
2025-04-27 21:20:04 +0300

    fix!: find gesture by id

--- src/main/java/com/netharus/domain/enums/Gestures.java
+++ src/main/java/com/netharus/domain/enums/Gestures.java
@@ -25,13 +25,16 @@ public enum Gestures {
     private final int id;
 
     public static String getGestureTitle(int id) {
-        return values()[id].getTitle();
+        return values()[id-1].getTitle();
     }
 
     public static String getGestures(String scenario) {
         List<Integer> gestureIds = Arrays.stream(scenario.trim().split("\\s+"))
                 .map(Integer::parseInt)
                 .toList();
+        return gestureIds.stream().map(Gestures::getGestureTitle).collect(Collectors.joining(","));
+    }
+    public static String getGestures(List<Integer> gestureIds) {
         return gestureIds.stream().map(Gestures::getGestureTitle).collect(Collectors.joining(","));
     }
 
